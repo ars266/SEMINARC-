0c646bc2d369600547d89e0459016f8d58ef765f
Albina Lipchey <albina20021991@mail.ru>
2022-06-12 14:26:55 +0300

    Домашнее задание к меминару 2

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -69,7 +69,7 @@ else
 
 
 
-int N, current;
+/*int N, current;
 
 Console.WriteLine("Введите число: ");
 N = Convert.ToInt32(Console.ReadLine());
@@ -81,3 +81,56 @@ while(N - 1 == 0)
 }
 current = N / 2;
 current ++;
+*/
+
+
+
+
+
+/*ДОМАШНЕЕ ЗАДАНИЕ К СЕМИНАРУ 2*/
+
+/*
+int CutNumer()
+{
+    int num = new Random().Next(100,999);
+Console.WriteLine($"Выбранное число {num}");
+
+int sot = num / 10;
+int result = sot % 10;
+return result;
+}
+*/
+
+
+
+/*
+int CutNotFhree ()
+{
+    int num, result;
+    Console.WriteLine("Введите любое целое число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num < 100) Console.WriteLine("Третья цифра отсутствует");
+    if (num > 100);
+    result = num / 10;
+    return result;
+}
+*/
+
+
+/*
+int WeekDays ()
+{
+    int num, result;
+    Console.WriteLine("Введите цифру дня недели от 1 до 7: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num >= 6)
+    {
+    return True;
+    }
+    else
+    {
+    return False;
+    }
+
+}
+*/
